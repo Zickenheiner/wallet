1a52603fa405e7e9342f3e1eb6588b39fb17ec61
Hakkenheiner <remi.zicken@gmail.com>
2026-03-30 20:02:09 +0200

    feat: ajout de la prise en charge de PWA avec vite-plugin-pwa et mise à jour du fichier de configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Mon App",
+        short_name: "MonApp",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#d41111",
+        theme_color: "#111111",
+        icons: [
+          {
+            src: "/binance.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/binance.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
